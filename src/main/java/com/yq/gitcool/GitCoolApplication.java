5b9617f2e7b403154a2461daf7bc4dc7a2f73eab
PaulYuQ <1632255693@qq.com>
2020-05-10 17:58:24 +0800

    你若安好，便是晴天。

---
+++ src/main/java/com/yq/gitcool/GitCoolApplication.java
@@ -0,0 +1,13 @@
+package com.yq.gitcool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitCoolApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GitCoolApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/yq/gitcool/GitCoolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yq.gitcool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitCoolApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
